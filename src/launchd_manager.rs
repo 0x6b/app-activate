7042d822b48193b6af504295f2036dd4bca1f9ea
kaoru <k@warpnine.io>
2024-10-29 23:07:37 +0900

    chore: code consistency

--- src/launchd_manager.rs
+++ src/launchd_manager.rs
@@ -57,8 +57,12 @@ encoding="UTF-8"?> <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://
 </plist>
 "#,
                                   self.name, self.bin.to_string_lossy()).as_bytes());
-        if run_cmd!(launchctl bootstrap gui/$id $plist).is_err() {
-            warn!("Failed to bootstrap {name}")
+
+        // Just record the result of each command and continue on error, because I'm not 100% sure
+        // what is the best way to register a service due to sparse documentation...
+        match run_cmd!(launchctl bootstrap gui/$id $plist) {
+            Ok(_) => info!("Bootstrapped gui/{id}/{name}"),
+            Err(why) => warn!("Failed to bootstrap gui/{id}/{name}: {why}"),
         }
         match run_cmd!(launchctl load -w $plist) {
             Ok(_) => info!("Registered app-activate"),
@@ -78,16 +82,20 @@ encoding="UTF-8"?> <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://
     pub fn unregister(&self) -> Result<()> {
         let name = self.name.clone();
         let plist = self.plist.clone();
-        if run_cmd!(launchctl stop $name).is_err() {
-            warn!("Failed to stop {name}")
+
+        // Just record the result of each command and continue on error, because I'm not 100% sure
+        // what is the best way to register a service due to sparse documentation...
+        match run_cmd!(launchctl stop $name) {
+            Ok(_) => info!("Stopped {name}"),
+            Err(why) => warn!("Failed to stop {name}: {why}"),
         }
-        if run_cmd!(launchctl unload -w $plist).is_err() {
-            warn!("Failed to unload {name}")
+        match run_cmd!(launchctl unload -w $plist) {
+            Ok(_) => info!("Unloaded {name}"),
+            Err(why) => warn!("Failed to unload {name}: {why}"),
         };
-        if remove_file(&plist).is_ok() {
-            info!("Removed {}", plist.to_string_lossy())
-        } else {
-            warn!("Failed to remove {}", plist.to_string_lossy())
+        match remove_file(&plist) {
+            Ok(_) => info!("Removed {plist:?}"),
+            Err(why) => warn!("Failed to remove {plist:?}: {why}"),
         }
         Ok(())
     }
